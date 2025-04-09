2234d2c2e36aa22680e99aa3f6f752252db73d64
oYuriyan <ymotoyuri@gmail.com>
2025-04-09 16:23:44 -0300

    mudança de nome de pasta

--- TG/wsgi.py
+++ TG/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TG.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'business_game.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TG.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'business_game.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
