79f8de4f17a494f26510051cb1cf5c3dd91e328b
oYuriyan <ymotoyuri@gmail.com>
2025-04-11 15:33:59 -0300

    lobby provisório

--- game/urls.py
+++ game/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from game.views import login, dashboard
+from game.views import lobby
 
 urlpatterns = [
     path('login/', login.login_view, name='login'),
     path('logout/', login.logout_view, name='logout'),
     path('dashboard/', dashboard.dashboard_view, name='dashboard'),
+    path('lobby/', lobby.lobby_view, name='lobby'),
 ]
