6d80634ec2a4dcfcb99d48c0ec7d4495ccfd1a25
VINICIUSSOARESDELIMA <viniciussoaresdelima5@gmail.com>
2025-05-22 12:14:41 -0300

    Criar produto consertado

--- game/models/produtos.py
+++ game/models/produtos.py
@@ -4,6 +4,7 @@ class Produto(models.Model):
     nome = models.CharField(max_length=255)
     preco = models.DecimalField(max_digits=8, decimal_places=2)
     quantidade = models.PositiveIntegerField()
+    categoria = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nome
